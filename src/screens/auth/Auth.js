b5a2457df4b3d3b627543515376a280783455ca8
YarynaN <yaryna.nepyivoda@gmail.com>
2018-11-10 17:38:41 +0200

    Navigation
    Setting up navigation

---
+++ src/screens/auth/Auth.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import{ View, Text } from 'react-native';
+
+class AuthScreen extends Component{
+	render () {
+		return(
+			<View>
+				<Text>Yarya is Awesome</Text>
+			</View>
+		);
+	}
+}
+
+export default AuthScreen;
