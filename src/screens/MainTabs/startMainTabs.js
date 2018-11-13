b93248542b46d6629eb98240d6fc0b0cc7b43a10
YarynaN <yaryna.nepyivoda@gmail.com>
2018-11-13 19:25:30 +0200

    code improvement. Titles added

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -2,52 +2,67 @@ import { Navigation } from "react-native-navigation";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const startTabs = () => {
-	Promise.all([
-		Icon.getImageSource("md-map", 30),
-		Icon.getImageSource("share-alt", 30)
-	]).then(sources => {
-		Navigation.setRoot({
-		root: {
-			bottomTabs: {
-			  children: [{
-			    stack: {
-			      children: [{
-			        component: {
-			          name: 'awesome-places.FindPlaceScreen',
-			          passProps: {
-			            text: 'Find Place'
-			          }
-			        }
-			      }],
-			      options: {
-			        bottomTab: {
-			          title: 'Find Place',
-			          icon: sources[0],
-			          testID: 'FIRST_TAB_BAR_BUTTON'
-			        }
-			      }
-			    }
-			  },
-			  {
-			    component: {
-			      name: 'awesome-places.SharePlaceScreen',
-			      passProps: {
-			        text: 'Share Place'
-			      },
-			      options: {
-			        bottomTab: {
-			          title: 'Share Place',
-			          icon: sources[1],
-			          testID: 'SECOND_TAB_BAR_BUTTON'
-			        }
-			      }
-			    }
-			  }]
-			}
-		}
-	});
-	});
-	
+    Promise.all([
+        Icon.getImageSource("md-map", 30),
+        Icon.getImageSource("share-alt", 30)
+    ]).then(sources => {
+        Navigation.setRoot({
+        root: {
+            bottomTabs: {
+              children: [
+              {
+                stack: {
+                  children: [{
+                    component: {
+                      name: 'awesome-places.FindPlaceScreen',
+                      passProps: {
+                        text: 'Find Place'
+                      },
+                      options: {
+                        topBar: {
+                          title: {
+                            text: 'Find Place',
+                          }
+                        }
+                      }
+                    }
+                  }],
+                  options: {
+                    bottomTab: {
+                      icon: sources[0]
+                    }
+                  }
+                }
+              },
+              {
+                stack: {
+                  children: [{
+                    component: {
+                      name: 'awesome-places.SharePlaceScreen',
+                      passProps: {
+                        text: 'Share Place'
+                      },
+                      options: {
+                        topBar: {
+                          title: {
+                            text: 'Share Place',
+                          }
+                        }
+                      }                      
+                    }
+                  }],
+                  options: {
+                    bottomTab: {
+                      icon: sources[1]
+                    }
+                  }
+                }
+              }]
+            }
+        }
+    });
+    });
+    
 }
 
 export default startTabs;
