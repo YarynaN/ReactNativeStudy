96a15076ad82f310c7937b0f36e688f100b09976
YarynaN <yaryna.nepyivoda@gmail.com>
2018-12-07 18:35:30 +0200

    Share place screen partial edit

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,9 +1,12 @@
 import React, {Component} from "react";
-import {View, Text} from "react-native";
+import { View, Text, TextInput, Button, StyleSheet, ScrollView, Image} from "react-native";
 import {connect} from "react-redux";
 
-import PlaceInput from "../../components/PlaceInput/PlaceInput";
 import { addPlace } from "../../store/actions/index";
+import DefaultInput from "../../components/UI/DefaultInput";
+import HeadingText from "../../components/UI/HeadingText";
+import MainText from "../../components/UI/MainText";
+import imagePlaceHolder from "../../assets/berlin_1.jpg";
 
 class SharePlaceScreen extends Component {
 	constructor(props){
@@ -27,17 +30,59 @@ class SharePlaceScreen extends Component {
 
 	render(){
 		return(
-			<View>
-				<PlaceInput onPlaceAdded={this.placeAddedHandler} />
+			<ScrollView>
+			<View style={styles.container}>
+				<MainText>
+					<HeadingText>Share a Place with us</HeadingText>
+				</MainText>
+				<View style={styles.placeholder}>
+					<Image source={imagePlaceHolder} style={styles.previewImage}/>
+				</View>
+				<View style={styles.button}>
+					<Button title="Pick Image"/>
+				</View>
+				<View style={styles.placeholder}>
+					<Text>Map</Text>
+				</View>
+				<View style={styles.button}>
+					<Button title="Locate Me"/>
+				</View>
+				<DefaultInput placeholder="Place Name"/>
+				<View style={styles.button}>
+					<Button title="Share The Place"/>
+				</View>
 			</View>
+			</ScrollView>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container:{
+		flex: 1,
+		alignItems: "center"
+	},
+	placeholder:{
+		borderWidth: 1,
+		borderColor: "black",
+		backgroundColor: "#eee",
+		width: "80%",
+		height: 150
+	},
+	button:{
+		margin: 8
+	},
+	previewImage:{
+		width: "100%",
+		height: "100%"
+	},
+});
 
 const mapDispatchTpProps = dispatch => {
 	return {
 		onAddPlace:(placeName) => dispatch (addPlace(placeName))
 	};
 };
+
 
 export default connect(null, mapDispatchTpProps)(SharePlaceScreen);
