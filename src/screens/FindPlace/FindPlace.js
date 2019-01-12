19919de9ab7d64bacef3bf869d4705dceed312cd
Yaryna <yaryna.napyivoda@gmail.com>
2019-01-12 12:57:03 +0200

    styling navigation buttons

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,10 +1,18 @@
 import React, {Component} from "react";
-import {View, Text} from "react-native";
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
 import {connect} from "react-redux"
 
 import PlaceList from "../../components/PlaceList/PlaceList"
 
 class FindPlaceScreen extends Component {
+	static navigatorStyle = {
+		navBarButtonColor: "orange"
+	};
+
+	state = {
+		placesLoaded: false
+	};
+
 	constructor(props){
 		super(props);
 		this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
@@ -20,6 +28,12 @@ class FindPlaceScreen extends Component {
 		}
 	}
 	
+	placesSearchHandler = () => {
+		this.setState({
+			placesLoaded: true 
+		});
+	};
+
 	itemSelectedHandler = key => {
 		const selPlace = this.props.places.find(place => {
 			return place.key === key;
@@ -33,13 +47,45 @@ class FindPlaceScreen extends Component {
 		});
 	};
 	render(){
-		return(
-			<View>
+		let content = (
+			<TouchableOpacity onPress={this.placesSearchHandler}>
+				<View style={styles.searchButton}>
+					<Text style={styles.searchButtonText}>Search Me</Text>
+				</View>
+			</TouchableOpacity>
+		);
+
+		if(this.state.placesLoaded){
+			content = (
 				<PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler}/>
+			);
+		}
+		return(
+			<View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+				{content}
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	buttonContainer:{
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	searchButton:{
+		borderColor: "orange",
+		borderWidth: 3,
+		borderRadius: 50,
+		padding: 20
+	},
+	searchButtonText:{
+		color: "orange",
+		fontWeight: "bold",
+		fontSize: 26
+	}
+});
 
 const mapStateToProps = state => {
 	return {
