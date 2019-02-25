63c0d8ab3937f2b82a0db7773a1c868b79d8403d
Yaryna <yaryna.napyivoda@gmail.com>
2019-02-25 19:04:04 +0200

    Back end, connecting to server
    Stroying images and information on server

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,11 +2,33 @@ import { ADD_PLACE, DELETE_PLACE} from './actionTypes'
 
 
 export const addPlace = (placeName, location, image) => {
-	return {
-		type: ADD_PLACE,
-		placeName: placeName,
-		location: location,
-		image: image
+	return dispatch => {
+		const placeData = {
+			name: placeName,
+			location: location
+		};
+
+		fetch("https://us-central1-reactnative-cour-1549562975674.cloudfunctions.net/storeImage", {
+			method: "POST",
+			body: JSON.stringify({
+				image: image.base64
+			})
+		})
+			.catch(err => console.log(err))
+			.then(res => res.json())
+			.then(parsedRes => {
+			console.log(parsedRes)
+			});
+
+		// fetch("https://reactnative-cour-1549562975674.firebaseio.com/places.json", {
+		// 	method: "POST",
+		// 	body: JSON.stringify(placeData)
+		// })
+		// .catch(err => console.log(err))
+		// .then(res => res.json())
+		// .then(parsedRes => {
+		// 	console.log(parsedRes)
+		// });
 	};
 };
 
@@ -17,3 +39,4 @@ export const deletePlace = (key) => {
 	};
 };
 
+// 
